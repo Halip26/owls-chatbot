57cef52ddfebdd9ce588fb393002ba5771f6c4d3
Halip26 <halipuddin.angko@gmail.com>
2023-04-17 17:25:01 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Creating machine learning for kids called Owls chatbot
+
+You can use this code to submit text to the machine learning model that you've trained.
+Enter the text:
+The text that you want to test
+
+Running this will print something like:
+
+```python
+$ python main.py
+  #result: 'food' with 81% confidence
+```
+
+[Click here for Python for adding a training example](https://machinelearningforkids.co.uk/)
+If you've never used [requests](https://realpython.com/python-requests/) before, you might need to [install it first](https://realpython.com/python-requests/#getting-started-with-requests).
+Ask your teacher for help if you're not sure how to do that.
---
+++ main.py
@@ -0,0 +1,45 @@
+import requests
+
+# Fungsi ini akan meneruskan teks pengguna ke model pembelajaran mesin
+# dan mengembalikan hasil teratas dengan probabilitas tertinggi
+def classify(text):
+    key = "8bbc7840-dcfb-11ed-809b-a3e71b9554887d93e513-00be-4933-8ce0-324db72e5afb"
+    url = "https://machinelearningforkids.co.uk/api/scratch/"+ key + "/classify"
+
+    response = requests.get(url, params={ "data" : text })
+
+    if response.ok:
+        responseData = response.json()
+        topMatch = responseData[0]
+        return topMatch
+    else:
+        response.raise_for_status()
+
+'''# GANTI INI menjadi sesuatu yang kamu inginkan untuk diklasifikasikan oleh model pembelajaran mesin kamu
+demo = classify("what foods do owls like?")
+
+label = demo["class_name"]
+confidence = demo["confidence"]
+
+
+# GANTI INI untuk melakukan sesuatu yang berbeda dengan hasilnya
+print ("result: '%s' with %d%% confidence" % (label, confidence))'''
+
+def answer_question():
+    question = input("> ")
+    answer = classify(question)
+    answerclass = answer["class_name"]
+    if answerclass == "food":
+      print ("invertebrates (such as insects, spiders, earthworms, snails and crabs), fish, reptiles, amphibians, birds and small mammals")
+    elif answerclass ==  "countries":
+      print ("")
+    elif answerclass ==  "lifespan":
+      print ("")
+    elif answerclass == "species":
+      print ("")
+    elif answerclass == "size":
+      print ("")
+
+print("what would you like to know about owls? ")
+while True:
+   answer_question()
